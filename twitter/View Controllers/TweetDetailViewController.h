b11787484cfa99c3ffaa57bf7a70dc9b1184220b
Olalekan Abdurazaq Adisa <adisaolalekan@adisaolalekan-mbp.dhcp.thefacebook.com>
2020-07-02 19:16:08 -0500

    Date Feature added

---
+++ twitter/View Controllers/TweetDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  TweetDetailViewController.h
+//  twitter
+//
+//  Created by Olalekan Abdurazaq Adisa on 7/2/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
